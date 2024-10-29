095f94e91785fde01cef448f336915b8343c7e21
Saja Ayesh <sajaayesh9@gmail.com>
2024-10-29 10:40:33 +0200

    assighment 7

--- Assets/githuptst/githup.cs
+++ Assets/githuptst/githup.cs
@@ -7,13 +7,37 @@ public class githup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(" hi sajaa");
-        Debug.Log(" hi ya saja");
-
-
-        Debug.Log("saja");
         
-    }
+        
+        int counter = 0;
+
+        while (counter < 25)
+        {
+           
+            int randomNumber = Random.Range(1, 21);
+
+            // If the number is 5, skip printing it 
+            if (randomNumber == 5)
+            {
+                continue;
+            }
+
+            // امر الطباعة بعد كونتيو لتخطي طباعة الرقم 5 
+            Debug.Log("Generated Number: " + randomNumber);
+
+            // If the number is 15, break to exit the loop
+            if (randomNumber == 15)
+            {
+                Debug.Log("Number 15 found! Exiting the loop.");
+                break;
+            }
+
+           
+            counter++;
+       }
+
+       } 
+    
 
     // Update is called once per frame
     void Update()
