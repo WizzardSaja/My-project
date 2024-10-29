3395d3af4bc1cf3b0defa06341073a03fd27abd8
Saja Ayesh <sajaayesh9@gmail.com>
2024-10-29 11:15:36 +0200

    change111

--- Assets/githuptst/ex2.cs
+++ Assets/githuptst/ex2.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class ex2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
          string[] words = { "Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
         string sentence = "";
         int wordCount = 0;
 
-        // While loop to build a sentence with 7 random words
+        
         while (wordCount < 7)
         {
-            // Pick a random word from the array
+            
             string randomWord = words[UnityEngine.Random.Range(0, words.Length)];
 
-            // Add the word to the sentence
+            
             sentence += randomWord + " ";
 
-            // Increment the word count
+            
             wordCount++;
         }
 
@@ -28,7 +28,7 @@ public class ex2 : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
