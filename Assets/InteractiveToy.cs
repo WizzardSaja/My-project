e44d495485f9cf082bc22d1f400664a6481d5b18
Saja Ayesh <sajaayesh9@gmail.com>
2024-12-03 02:05:28 +0200

    toyy

---
+++ Assets/InteractiveToy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public float moveSpeed = 5f;    // سرعة الحركة
+    public float scaleSpeed = 0.5f; // سرعة تغيير الحجم
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // تحريك الكائن باستخدام الأسهم
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(moveX, moveY, 0);
+  
+    }
+}
